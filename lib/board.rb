b67b52da60e04f6ff3e4930f1303372a09a26759
philomory <philomory@localhost>
2010-05-24 11:40:22 -1000

    Full checkers board implementation: moving, jumping, kings, mandatory jump checking, edges, two colors; includes full test suite.

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,5 @@
 class Board
-
-
-  
+  class MoveError < StandardError; end
   DEFAULT_BOARD_STRING = "wwwwwwwwwwww________bbbbbbbbbbbb"
   DEFAULT_PADDED_BOARD_STRING = "*****bbbb*bbbbbbbb*________*wwwwwwww*wwww*****"
   # convert padded to unpadded: str.delete('*')
@@ -12,6 +10,10 @@ class Board
     raise(ArgumentError,"Board requires a board string of length 32") unless input_string.length == 32
     raise(ArgumentError,"Board string can only contain w, W, b, B and _") if input_string =~ /[^wWbB_]/
     @padded_board_string = pad_string(input_string)
+  end
+  
+  def initialize_copy(original)
+    @padded_board_string = original.padded_board_string.dup
   end
   
   def rank_and_file(rank,file)
@@ -29,19 +31,145 @@ class Board
   #
   # Ask whether the given piece can move in the specified direction.
   def can_move?(*args)
-    index,dir = case args.length
-    when 2 then args
-    when 3 then [padded_index(args[0],args[1]),args[2]]
-    else raise(ArgumentError,"Must provide ")
-    end
+    index,dir = process_move_args(*args)
     self[index].travels?(dir) && self[index + MOVE[dir]].empty?
   end
   
+  def do_move(*args)
+    raise(MoveError, "Requested move is not legal!") unless can_move?(*args)
+    index, dir = process_move_args(*args)
+    @padded_board_string[index + MOVE[dir]] = chr(index)
+    @padded_board_string[index] = :_
+    index + MOVE[dir]
+  end
   
+  def can_jump?(*args)
+    index, dir = process_move_args(*args)
+    jumper = self[index]
+    victim = self[index + MOVE[dir]]
+    target = self[index + 2*MOVE[dir]] 
+    jumper.travels?(dir) && victim.enemy?(jumper) && target.empty?
+  end
   
+  def do_jump(*args)
+    raise(MoveError, "Requested jump is not legal!") unless can_jump?(*args)
+    index, dir = process_move_args(*args)
+    @padded_board_string[index + 2*MOVE[dir]] = chr(index)
+    @padded_board_string[index] = :_
+    @padded_board_string[index + MOVE[dir]] = :_
+  end
+    
+  def can_multi_jump?(*args)
+    !!check_multi_jump(*args)
+  end
   
+  def do_multi_jump(*args)
+    result, new_index = check_multi_jump(*args)
+    if (result)
+      @padded_board_string = result.padded_board_string
+      new_index
+    else
+      raise(MoveError, "Requested series of jumps is not legal!")
+    end
+  end
+  
+  def can_take_turn?(*args)
+    color,index,action = process_turn_args(*args)
+    return false unless self[index].color == color
+    if (dir = action[:move])
+      can_move?(index,dir) and not jump_available?(color)
+    else
+      dirs = Array(action[:jump])
+      board, final_index = check_multi_jump(index,*dirs)
+      board and not [:nw,:ne,:sw,:se].any? {|dir| board.can_jump?(final_index,dir) }
+    end
+  end
+  
+  def do_take_turn(*args)
+    raise(MoveError,"Request turn is not legal!") unless can_take_turn?(*args)
+    _,index,action = process_turn_args(*args)
+    new_index = if (dir = action[:move])
+      do_move(index,dir)
+    else
+      dirs = Array(action[:jump])
+      do_multi_jump(index,*dirs)
+    end
+    crown_if_needed(new_index)
+  end
+  
+  def to_s
+    position_string
+  end
+    
+  # A compact string suitable for passing to Board.new to regenerate
+  # the current board.  
+  def position_string
+    # Oddly, String#delete is non-destructive, unlike Array#delete
+    @padded_board_string.delete('*').scan(/..../).reverse.join
+  end
+  
+  def ==(other)
+    position_string == other.position_string
+  end
+  
+  protected
+  def padded_board_string
+    @padded_board_string.dup
+  end  
   
   private
+  def process_move_args(*args)
+    case args.length
+    when 2 then args
+    when 3 then [padded_index(args[0],args[1]),args[2]]
+    else raise(ArgumentError)
+    end
+  end
+  
+  def process_multi_args(*args)
+    if !(args[0].is_a? Integer)
+      raise(ArgumentError,"Must pass index or rank and file, and a series of symbols")
+    elsif args[1].is_a? Symbol
+      [args[0],args[1..-1]]
+    elsif args[1].is_a? Integer
+      [padded_index(args[0],args[1]), args[2..-1]]
+    else
+      raise(ArgumentError,"Must pass index or rank and file, and a series of symbols")
+    end
+  end
+  
+  def process_turn_args(color,*args)
+    raise(ArgumentError) unless [:white,:black].include?(color)
+    index, action = if (args.length == 2 && args[0].kind_of?(Integer) && args[1].kind_of?(Hash))
+      args
+    elsif (args.length == 3 && args[0].kind_of?(Integer) && args[1].kind_of?(Integer) && args[2].kind_of?(Hash))
+      [padded_index(args[0],args[1]),args[2]]
+    else
+      raise(ArgumentError)
+    end
+    raise(ArgumentError) unless action[:move] || action[:jump]
+    raise(ArgumentError) if action[:move] && action[:jump]
+    [color,index,action]
+  end
+  
+  def check_multi_jump(*args)
+    index, dirs = process_multi_args(*args)
+    if dirs.empty?
+      false
+    else
+      ary = [self.dup,index]
+      dirs.inject(ary) do |ary,dir|
+        b,i = *ary
+        if b.can_jump?(i,dir)
+          b.do_jump(i,dir)
+          [b,i+2*MOVE[dir]]
+        else
+          return nil
+        end
+      end
+    end
+  end
+  
   def padded_index(rank,file)
     37 - (rank * 4.5).ceil + file
   end
@@ -50,19 +178,30 @@ class Board
     @padded_board_string[index]
   end
   
+  def jump_available?(color)
+    (0...46).to_a.any? do |index|
+      sq = self[index]
+      sq.color == color && [:ne,:se,:nw,:sw].any? {|dir| can_jump?(index,dir) }
+    end
+  end
+  
+  def crown_if_needed(index)
+    if self[index].white_pawn? && index.between?(5,8) # bottom row
+      @padded_board_string[index] = :W
+    elsif self[index].black_pawn? && index.between?(37,40) # top row
+      @padded_board_string[index] = :B
+    end
+  end
+  
   # Only any good with string of length 32
   def pad_string(str)
     str.scan(/..../).reverse.join.dup.insert(0,'*****').insert(9,'*').insert(18,'*').insert(27,'*').insert(36,'*').insert(41,'*****')
   end
   
-  # Only meant for strings of length 46
-  # def unpad(str = @padded_board_string)
-  #   str.delete('*') # oddly, String#delete is nondestructive, unlike Array#delete
-  # end
-  
   Square = Struct.new(:chr,:name,:state,:color,:crown) do
     def white?; color == :white; end
     def black?; color == :black; end
+    def enemy?(other); (black? && other.white?) || (white? && other.black?); end
     def  king?; crown; end
     def  pawn?; occupied? and not king?; end
     def empty?; state == :empty; end
--- test/unit/board_test.rb
+++ test/unit/board_test.rb
@@ -6,7 +6,6 @@ class BoardTest < ActiveSupport::TestCase
     @board = Board.new
   end
   
-  
   # ===============
   # = Setup tests =
   # ===============
@@ -110,24 +109,14 @@ class BoardTest < ActiveSupport::TestCase
   end
   
   test "cannot move off the edge of the board" do
-    bs = <<-END_OF_BOARD
-      W W W W
-     B _ _ _
-      _ _ _ W
-     B _ _ _
-      _ _ _ W
-     B _ _ _
-      _ _ _ W
-     B B B B
-    END_OF_BOARD
-    board = Board.new(bs)
+    board = edge_test_board
     moves = {
       [0,0] => :nw, [0,0] => :ne, [0,1] => :nw, [0,1] => :ne,
-      [0,2] => :nw, [0,2] => :ne, [0,3] => :nw, [0,3] => :ne,
+      [0,2] => :nw, [0,2] => :ne, [0,3] => :nw, [0,3] => :ne, [0,3] => :se,
       [1,0] => :nw, [1,0] => :sw, [3,0] => :nw, [3,0] => :sw,
       [5,0] => :nw, [5,0] => :sw, [2,3] => :ne, [2,3] => :se,
       [4,3] => :ne, [4,3] => :se, [6,3] => :ne, [6,3] => :se,
-      [7,0] => :sw, [7,0] => :se, [7,1] => :sw, [7,1] => :se,
+      [7,0] => :nw, [7,0] => :sw, [7,0] => :se, [7,1] => :sw, [7,1] => :se,
       [7,2] => :sw, [7,2] => :se, [7,3] => :sw, [7,3] => :se
     }
     moves.each_pair do |sq,dir|
@@ -136,9 +125,440 @@ class BoardTest < ActiveSupport::TestCase
     end  
   end
   
+  test "moving takes you to the destination square" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ w _ _
+      _ _ _ _
+     _ _ _ _
+      _ _ _ _
+     _ _ _ _
+      _ _ _ _
+     _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_move(1,1,:se)
+    assert board.rank_and_file(1,1).empty?
+    assert board.rank_and_file(2,1).white_pawn?
+  end
+  
+  # =================
+  # = Jumping Tests =
+  # =================
+  
+  test "can ask about jumping using index or rank and file" do
+    assert_nothing_raised { @board.can_jump?(0,0,:nw) }
+    assert_nothing_raised { @board.can_jump?(0,:ne)   }
+    assert_raises(ArgumentError) { @board.can_jump?(:ne) }
+  end
+  
+  test "an empty square cannot jump" do
+    [:ne,:nw,:se,:sw].each do |dir|
+      refute @board.can_jump?(3,0,dir), "Empty spaces shouldn't jump!"
+    end
+  end
+  
+  test "a pawn can jump forwards" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ _ w _
+      _ b b _
+     _ _ _ _
+      _ _ _ _
+     _ w w _
+      _ b _ _
+     _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+    assert board.can_jump?(1,2,:sw), "White pawn should jump southwest"
+    assert board.can_jump?(1,2,:se), "White pawn should jump southeast"
+    assert board.can_jump?(6,1,:nw), "Black pawn should jump northwest"
+    assert board.can_jump?(6,1,:ne), "Black pawn should jump northeast"
+  end
+    
+  test "a pawn cannot jump backwards" do
+    board = Board.new("____ bbbb __w_ ____ ____ _b__ wwww ____")
+    refute board.can_jump?(2,2,:nw), "White pawn should not jump northwest"
+    refute board.can_jump?(2,2,:ne), "White pawn should not jump northeast"
+    refute board.can_jump?(5,1,:sw), "Black pawn should not jump southwest"
+    refute board.can_jump?(5,1,:se), "Black pawn should not jump southeast"
+  end
+    
+  test "a king jumps in any direction" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ b b _
+      _ W _ _
+     _ b b _
+      _ _ _ _
+     _ _ _ _
+      _ _ _ _
+     _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+    
+    assert board.can_jump?(2,1,:nw), "King should jump northwest"
+    assert board.can_jump?(2,1,:ne), "King should jump northeast"
+    assert board.can_jump?(2,1,:sw), "King should jump southwest"
+    assert board.can_jump?(2,1,:se), "King should jump southeast"
+  end
+  
+  test "a jump must land on an empty square" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ _ w _
+      _ b b _
+     _ b _ w
+      _ _ _ _
+     _ _ _ _
+      _ _ _ _
+     _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+  
+    refute board.can_jump?(1,2,:se), "a jump must land in an empty square"
+    refute board.can_jump?(1,2,:sw), "a jump must land in an empty square"
+  end
+  
+  test "a piece cannot jump over an empty square" do
+    board = Board.new("____ ____ __w_ ____ ____ ____ ____ ____")
+    refute board.can_jump?(2,2,:sw), "should not jump over an empty square"
+  end
+  
+  test "a piece can only jump over another piece of a different color" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ _ w _
+      _ b w _
+     _ _ _ _
+      _ _ _ _
+     _ b w _
+      _ b _ _
+     _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+    assert board.can_jump?(1,2,:sw), "White should jump over black"
+    refute board.can_jump?(1,2,:se), "White should not jump over white"
+    refute board.can_jump?(6,1,:nw), "Black should not jump northwest"
+    assert board.can_jump?(6,1,:ne), "Black should jump over white"
+  end
+  
+  test "a piece cannot jump off the edge of the board" do
+    board = edge_test_board
+    outer_moves = {
+      [0,0] => :nw, [0,0] => :ne, [0,1] => :nw, [0,1] => :ne,
+      [0,2] => :nw, [0,2] => :ne, [0,3] => :nw, [0,3] => :ne, [0,3] => :se,
+      [1,0] => :nw, [1,0] => :sw, [3,0] => :nw, [3,0] => :sw,
+      [5,0] => :nw, [5,0] => :sw, [2,3] => :ne, [2,3] => :se,
+      [4,3] => :ne, [4,3] => :se, [6,3] => :ne, [6,3] => :se,
+      [7,0] => :nw, [7,0] => :sw, [7,0] => :se, [7,1] => :sw, [7,1] => :se,
+      [7,2] => :sw, [7,2] => :se, [7,3] => :sw, [7,3] => :se
+    }
+    
+    inner_moves = {
+      [1,1] => :nw, [1,1] => :ne, [1,2] => :nw, [1,2] => :ne,
+      [1,3] => :nw, [1,3] => :ne, [1,3] => :se,
+      [2,0] => :nw, [2,0] => :sw, [4,0] => :nw, [4,0] => :sw,
+      [3,3] => :ne, [3,3] => :se, [5,3] => :ne, [5,3] => :se, 
+      [6,0] => :nw, [6,0] => :sw, [6,0] => :se, [6,1] => :sw, 
+      [6,1] => :se, [6,2] => :sw, [6,2] => :se
+    }
+    outer_moves.each_pair do |sq,dir|
+      r, f = *sq
+      refute board.can_jump?(r,f,dir), "Piece at (#{r},#{f}) shouldn't jump #{dir}!"
+    end
+    inner_moves.each_pair do |sq,dir|
+      r, f = *sq
+      refute board.can_jump?(r,f,dir), "Piece at (#{r},#{f}) shouldn't jump #{dir}!"
+    end  
+  end
+  
+  test "jumping takes you to the destination square" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ w _ _
+      _ b _ _
+     _ _ _ _ ________________
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_jump(1,1,:se)
+    assert board.rank_and_file(1,1).empty?, "white pawn should no longer be in this square!"
+    assert board.rank_and_file(3,2).white_pawn?, "white pawn should be in this square now!"
+  end
+  
+  test "jumping removes the piece you jumped over" do
+    bs = <<-END_OF_BOARD
+      _ _ _ _
+     _ w _ _
+      _ b _ _
+     _ _ _ _ ________________
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_jump(1,1,:se)
+    assert board.rank_and_file(2,1).empty?, "black pawn should have been removed!"
+  end
+  
+  # =======================
+  # = Multi-jumping Tests =
+  # =======================
+  
+  test "can ask about jumping in sequence using index or rank and file" do
+    assert_nothing_raised("should multi-jump with rank and file") { @board.can_multi_jump?(0,0,:sw,:sw,:sw)    }
+    assert_nothing_raised("should multi-jump with index") { @board.can_multi_jump?(0,:se,:se,:se)      }
+    assert_raises(ArgumentError, "should raise argument error without any form of index") { @board.can_multi_jump?(:se,:se,:se) }
+  end
+  
+  test "can jump over many squares" do
+    bs = <<-END_OF_BOARD
+        _ W _ _
+       _ b b _
+        _ _ _ _
+       _ b b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    assert board.can_multi_jump?(0,1,:sw,:se,:ne,:nw), "should jump over many squares"
+  end
+  
+  test "cannot multi-jump over zero squares" do
+    board = Board.new('__w_____________________________')
+    refute board.can_multi_jump?(0,2)
+  end
+  
+  test "cannot re-jump over the same square twice" do
+    bs = <<-END_OF_BOARD
+        _ W _ _
+       _ b b _
+        _ _ _ _
+       _ b b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    refute board.can_multi_jump?(0,1,:sw,:se,:ne,:nw,:sw), "Should not jump over the same square twice (the victim should have been removed by now)"    
+  end
+  
+  test "a multi-jump should move the jumper to its final destination" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_multi_jump(0,3,:sw,:sw)
+    assert board.rank_and_file(0,3).empty?, "initial position should be empty"
+    assert board.rank_and_file(2,2).empty?, "intermediate position should be empty"
+    assert board.rank_and_file(4,1).white_pawn?, "white pawn should have arrived at final square"
+  end
+  
+  test "a multi-jump should remove all the victims" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_multi_jump(0,3,:sw,:sw)
+    assert board.rank_and_file(1,3).empty?, "first victim should be removed"
+    assert board.rank_and_file(3,2).empty?, "second victim should be removed"
+  end
+  
+  test "should not in fact jump zero squares" do
+    board = Board.new('__w_____________________________')
+    assert_raises(Board::MoveError) { board.do_multi_jump(0,2) }
+  end
+  
+  # ===================
+  # = Full Turn Tests =
+  # ===================
+  
+  test "can ask about legal turns using index or rank and file" do
+    assert_nothing_raised { @board.can_take_turn?(:white,0,0,:move => :sw)    }
+    assert_nothing_raised { @board.can_take_turn?(:white,0,:move => :sw)      }
+    assert_raises(ArgumentError) { @board.can_take_turn?(:white,:move => :sw) }
+  end
+  
+  test "must provide color when asking about turns" do
+    assert_raises(ArgumentError) { @board.can_take_turn?(0,0,:move => :sw) }
+  end
+  
+  test "must provide either move or jump arg, but not both" do
+    assert_nothing_raised { @board.can_take_turn?(:white,0,0,:move => :sw) }
+    assert_nothing_raised { @board.can_take_turn?(:white,0,0,:jump => :sw) }
+    assert_raises(ArgumentError) { @board.can_take_turn?(:white,0,0,:foo => :bar) }
+    assert_raises(ArgumentError) { @board.can_take_turn?(:white,0,0,:move => :sw, :jump => :nw) }
+  end
+  
+  test "color of the player must match the color of the piece being moved" do
+    bs = <<-END_OF_BOARD
+        _ _ _ _
+       _ w _ _
+        _ _ _ _
+       _ _ b _ ________________
+    END_OF_BOARD
+    board = Board.new(bs)
+    refute board.can_take_turn?(:white,3,2,:move => :nw), "White should not move black piece"
+    refute board.can_take_turn?(:black,1,1,:move => :se), "Black should not move white piece"
+  end
+  
+  test "a legal move comes back true" do
+    board = Board.new('w_______________________________')
+    assert board.can_take_turn?(:white,0,0,:move => :se), "should be able to move this way"
+  end
+  
+  test "an illegal move comes back false" do
+    board = Board.new('w_______________________________')
+    refute board.can_take_turn?(:white,0,0,:move => :ne), "should not be able to move that way"
+  end
+  
+  test "a legal jump comes back true" do
+    bs = <<-END_OF_BOARD
+        _ _ w _
+       _ _ b _
+        _ _ _ _ ____________________
+    END_OF_BOARD
+    board = Board.new(bs)
+    assert board.can_take_turn?(:white,0,2,:jump => :sw)
+  end
+  
+  test "an illegal jump comes back false" do
+    board = Board.new('w_______________________________')
+    refute board.can_take_turn?(:white,0,0,:jump => :se)
+  end
+  
+  test "a legal series of jumps comes back true" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    assert board.can_take_turn?(:white,0,3,:jump => [:sw,:sw])
+  end
+  
+  test "an illegal series of jumps comes back false" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    refute board.can_take_turn?(:white,0,3,:jump => [:sw,:se]), "this move should be illegal"
+  end
+  
+  test "when jumping, you cannot stop if there are still jumps available to continue with" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    refute board.can_take_turn?(:white,0,3,:jump => :sw), "should require the jump to be continued"
+  end
+  
+  test "cannot make a normal move if a jump is available" do
+    bs = <<-END_OF_BOARD
+        _ _ w _
+       _ _ _ b
+        _ _ _ _ ____________________
+    END_OF_BOARD
+    board = Board.new(bs)
+    refute board.can_take_turn?(:white,0,2,:move => :sw), "should not be able to move if jump is available"
+  end
+  
+  test "taking at turn should move your piece" do
+    board = Board.new('w_______________________________')
+    board.do_take_turn(:white,0,0,:move => :sw)
+    assert board.rank_and_file(0,0).empty?, "piece should not be here any more"
+    assert board.rank_and_file(1,0).white_pawn?, "piece should be here now"
+  end
+  
+  test "taking a turn should jump your piece" do
+    bs = <<-END_OF_BOARD
+        _ _ _ w
+       _ _ _ b
+        _ _ _ _
+       _ _ b _
+        _ _ _ _ ____________
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_take_turn(:white,0,3,:jump => [:sw,:sw])
+    assert board.rank_and_file(0,3).empty?
+    assert board.rank_and_file(1,3).empty?
+    assert board.rank_and_file(2,2).empty?
+    assert board.rank_and_file(3,2).empty?
+    assert board.rank_and_file(4,1).white_pawn?
+  end
+  
+  test "moving to the end of the board should make a pawn a king" do
+    board = Board.new('______b_________________________')
+    board.do_take_turn(:black,1,2,:move => :ne)
+    assert board.rank_and_file(0,2).black_king?, "pawn should have been kinged"
+  end
+  
+  test "jumping to the end of the board should make a pawn a king" do
+    bs = <<-END_OF_BOARD
+         _ _ _ _
+        _ _ _ _
+         _ _ _ _
+        _ _ _ _
+         _ _ _ _
+        _ _ w _
+         _ _ b _
+        _ _ _ _
+    END_OF_BOARD
+    board = Board.new(bs)
+    board.do_take_turn(:white,5,2,:jump => :se)
+    assert board.rank_and_file(7,3).white_king?, "pawn should have been kinged"
+  end
   
   
+  # ======================
+  # = Miscelaneous tests =
+  # ======================
   
+  test "a board can give its position as a string" do
+    bs = 'w__W________________________B__b'
+    board = Board.new(bs)
+    assert_equal bs, board.position_string
+  end
+  
+  test "boards with equal positions are equal" do
+    bs = 'w__W________________________B__b'
+    board1 = Board.new(bs)
+    board2 = Board.new(bs)
+    assert (board1 == board2), "boards in equal positions should be consider equal"
+  end
+  
+  test "a duplicate board begins in the same position" do
+    board1 = Board.new('w__W________________________B__b')
+    board2 = board1.dup
+    assert_equal board1, board2
+  end
+  
+  test "a duplicate board changes independently of the original" do
+    bs = 'w__W________________________B__b'
+    board1 = Board.new(bs)
+    board2 = board1.dup
+    
+    board2.do_move(0,0,:se)
+    refute_equal board1, board2, "board2 should no longer look like board 1"
+    assert_equal bs, board1.position_string, "board1 should not have changed at all"
+  end
+
+  
+  # ====================
+  # = Helper functions =
+  # ====================
   
   def rank_range(range,board=@board,&block)
     range.to_a.each {|r| whole_rank(r,board,&block) }
@@ -149,5 +569,19 @@ class BoardTest < ActiveSupport::TestCase
       yield board.rank_and_file(rank,file)
     end
   end
-
+  
+  def edge_test_board
+    bs = <<-END_OF_BOARD
+      W W W W
+     W B B B
+      B _ _ W
+     W _ _ B
+      B _ _ W
+     W _ _ B
+      B B B W
+     W W W W
+    END_OF_BOARD
+    Board.new(bs)
+  end
+  
 end
