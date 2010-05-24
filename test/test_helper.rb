880ea7aa5e0d21a9baf4b0ef1cb22d06bb2e549e
philomory <philomory@localhost>
2010-05-24 11:38:46 -1000

    Some random hackery to allow testing to be done with an in-memory sqlite database rather than an on-disk one.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,24 +16,16 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
+  if in_memory_database?
+    puts "Setting up in memory database"
+    old_stdout = $stdout
+    $stdout = open('/dev/null','w')
+    load "#{Rails.root}/db/schema.rb"
+    $stdout.close
+    $stdout = old_stdout
+  end
   Dir[File.expand_path('../blueprints/*.rb',__FILE__)].each {|f| require f}
 end
-
-# --- Instructions ---
-# - Sort through your spec_helper file. Place as much environment loading 
-#   code that you don't normally modify during development in the 
-#   Spork.prefork block.
-# - Place the rest under Spork.each_run block
-# - Any code that is left outside of the blocks will be ran during preforking
-#   and during each_run!
-# - These instructions should self-destruct in 10 seconds.  If they don't,
-#   feel free to delete them.
-#
-
-
-
-
-
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
