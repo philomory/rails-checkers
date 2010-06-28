5883ccbb8023446337341e0b435a982fd101499c
philomory <philomory@localhost>
2010-06-28 12:24:20 -1000

    Removed a bogus migration: that field already existed in the database!

--- db/migrate/20100624211935_add_recipient_to_invitation.rb
+++
@@ -1,9 +0,0 @@
-class AddRecipientToInvitation < ActiveRecord::Migration
-  def self.up
-    add_column :invitations, :recipient, :references
-  end
-
-  def self.down
-    remove_column :invitations, :recipient
-  end
-end
